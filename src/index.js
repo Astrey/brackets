b2e6a5681b6e7e8cbd153789155d1583502749dd
Anna Glukhova <ann.astrey@gmail.com>
2022-07-16 00:17:02 +0300

    feat: length fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
-  let length = ((str.length)/2)
+  const length = ((str.length)/2)
   for (let i=0; i<=length; ++i)
   (bracketsConfig.map(pair => pair.join(''))).forEach(pair => str = str.replace(pair, ''));
   return (str == '') ? true : false;
